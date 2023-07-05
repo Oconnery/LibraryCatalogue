e6e128845d0492ed3fd937f705bb1bbfef491ea4
Oisin <oisinconnery@hotmail.co.uk>
2023-07-05 09:47:59 +0100

    JX-10: Add Util unit tests. Fix numberOfDigits method.

--- src/main/java/com/library/catalogue/util/Util.java
+++ src/main/java/com/library/catalogue/util/Util.java
@@ -2,7 +2,12 @@ package com.library.catalogue.util;
 
 public class Util {
     public static Integer numberOfDigits(Number number) {
-        double result = (Math.log10(number.doubleValue()) + 1);
+        if (number.equals(0))
+            return 1;
+        double numberAsDouble = number.doubleValue();
+        if (number.doubleValue() < 0)
+            numberAsDouble = -numberAsDouble;
+        double result = (Math.log10(numberAsDouble) + 1);
         return (int) result;
     }
 }
